0b7753b0d053089d3d1bab78285a79f3fdfdd4ea
Thiago Cunha Ferreira <cunhathiagoferreira@gmail.com>
2023-09-30 19:09:06 -0300

    aguardando envio de modelo para continuação do projeto

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+'use strict';
+const SimularButton = document.getElementById('btn_simular')
+console.log('JS carregado')
+
+SimularButton.addEventListener('click', () => {
+    const FormInputs = {
+        valor : document.getElementById('valor').value,
+        banco : document.getElementById('banco').value,
+        meses : document.getElementById('tempo').value
+    }
+    console.log(FormInputs)
+    alert('Ola mundo!')
+})
 //alert('Ola Mundo!');
