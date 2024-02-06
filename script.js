d693317d573432a6299b1b71919bdcca982a4023
Thiago Cunha Ferreira <cunhathiagoferreira@gmail.com>
2024-02-06 00:35:27 -0300

    Added taxes and values

--- script.js
+++ script.js
@@ -7,18 +7,46 @@ const Simulador = document.getElementsByClassName('simulado')[0]
 let elementValorParcelas = document.getElementById('valor_parcelas');
 let elementValorTotal = document.getElementById('valor_total');
 
+var ValorCarroFixo = 103000
+
+var taxaBanco = [
+    {nome : 'banco_brasil', taxa : 1.13},
+    {nome : 'santander', taxa : 1.12},
+    {nome : 'caixa', taxa : 1.03},
+]
+var valorMeses = [
+    {tempo: 12, taxa: 3},
+    {tempo: 36, taxa: 6},
+    {tempo: 48, taxa: 12}
+]
+
 SimularButton.addEventListener('click', () => {
     const FormInputs = {
         valor : document.getElementById('valor').value,
         banco : document.getElementById('banco').value,
         meses : document.getElementById('tempo').value
     }
-    console.log(FormInputs)
+    
+    if(FormInputs.meses == 'null' || FormInputs.valor == "" || FormInputs.banco == 'null'){
+        alert('Você precisa preencher todos os campos!')
+        return
+    }
+    var mesesNumero = Number(FormInputs.meses)
+    var valorNumero = Number(FormInputs.valor)
+    console.log(valorNumero)
+    if(isNaN(valorNumero)){
+        alert('No campo de valor, deve-se informar apenas numeros!')
+        return
+    }
+    
     MainClass.style.display = 'none'
     Simulador.style.display = 'flex'
 
-    let valorParcelas = 1302.23
-    let valorTotal = 62507.04
+    let jurosBanco = taxaBanco.find(e => e.nome == FormInputs.banco).taxa
+    let jurosTempo = valorMeses.find(e => e.tempo == mesesNumero).taxa
+
+    let valorTotal = ((ValorCarroFixo-valorNumero)*jurosBanco)*jurosTempo
+    let valorParcelas = valorTotal/mesesNumero
 
     let atualParcelas = 0
     let atualTotal = 0
